4dd46e0dfd21f8c1fbc07463350b6b38afd5a758
nehanaumankhan <nehanaumankhan@gmail.com>
2025-09-25 23:05:45 +0500

    Add comment for clarity on common operations in ALU module

--- LAB5/alu.v
+++ LAB5/alu.v
@@ -6,7 +6,7 @@ module alu (
 );
 localparam ADD = 3'b000,
            SUB = 3'b001,
-           AND = 3'b010
+           AND = 3'b010,
            OR  = 3'b011,    
            XOR = 3'b100,
            NOT = 3'b101,
@@ -45,7 +45,7 @@ always @(*) begin
 		LSL: res = op1 << 1;
 		LSR: res = op1 >> 1;
 	endcase 
-
+	//common for all operations
 	z_flag = (res == '0);
 	s_flag = res[7];
 end
