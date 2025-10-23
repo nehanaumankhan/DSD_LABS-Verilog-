63cfc4a23f5393e3b93802b882cd841f662116d8
nehanaumankhan <nehanaumankhan@gmail.com>
2025-10-23 20:49:40 +0500

    Refactor RAM 16x4 module to use bidirectional data bus and improve output control

--- LAB 8/RAM_16x4/ram_16x4.v
+++ LAB 8/RAM_16x4/ram_16x4.v
@@ -1,27 +1,29 @@
 module ram_16x4(
-    input       clk, rst,
-    input       read_enable, write_enable, chip_select,
-    input       [3:0] address, //2^4 = 16 addresses
-    input       [3:0] d_in,    //4-bits data input
-    output reg  [3:0] d_out    //4-bits data output
+    input        clk, rst,
+    input        read_enable, write_enable, chip_select,
+    input  [3:0] address,
+    inout  [3:0] data   // Bidirectional data bus
 );
-// 16 locations each of 4-bits width
-reg [3:0] buffer [0:15];
+
+reg [3:0] buffer [0:15]; // 16 locations, each 4 bits
+reg [3:0] temp_data;      // Holds data during read
 integer i;
-always@(posedge clk or posedge rst) 
-    begin
-        if (rst) begin
-            for (i = 0; i < 16; i = i + 1) begin
-                buffer[i] <= '0;
-            end
-            d_out <= '0;
-        end
-        else if (chip_select && write_enable) 
-            buffer[address] <= d_in;
-        else if (chip_select && read_enable)
-            d_out <= buffer[address];
-        else
-            d_out <= 4'bzzzz;
+
+// Output control (Tri-state buffer)
+assign data = (chip_select && read_enable && !write_enable) ? temp_data : 4'bz;
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        for (i = 0; i < 16; i = i + 1)
+            buffer[i] <= 4'b0000; // Reset all memory
+        temp_data <= 4'b0000;
     end
+    else if (chip_select && write_enable && !read_enable) begin
+        buffer[address] <= data;  // Write to memory
+    end
+    else if (chip_select && read_enable && !write_enable) begin
+        temp_data <= buffer[address]; // Read from memory
+    end
+end
 
 endmodule
